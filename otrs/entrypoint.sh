b0c0fc924adfe120a95ff67aff4579a64884b3e4
Mauro Michielon - EEA 06Oct2015 <michimau@kvm-dev5-06.pdmz.eea>
2020-04-02 14:51:23 +0200

    cluster version finalization

--- otrs/entrypoint.sh
+++ otrs/entrypoint.sh
@@ -4,8 +4,8 @@ mysqlcmd="mysql -uroot -hmariadb -p$MYSQL_ROOT_PASSWORD "
 
 while true; do
   out="`$mysqlcmd -e "SELECT COUNT(*) FROM mysql.user;" 2>&1`"
-  echo -e $out
-  echo "$out" | grep "COUNT"
+#  echo -e $out
+#  echo "$out" | grep "COUNT"
   if [ $? -eq 0 ]; then
     #echo -e "\n\e[92mServer is up !\e[0m\n"
     break
@@ -24,38 +24,24 @@ sed "s#ldap_host#$LDAP_HOST#g" -i ${OTRS_ROOT}Kernel/Config.pm
 sed "s#host_ip_address#$SERVER_NAME#g" -i /etc/httpd/conf.d/zzz_otrs.conf
 sed "s#User apache#User otrs#g" -i /etc/httpd/conf/httpd.conf
 
-$mysqlcmd -e 'use otrs'
-if [ $? -ne 0  ]; then
-  $mysqlcmd -e "CREATE DATABASE IF NOT EXISTS otrs;"
-  $mysqlcmd -e " GRANT ALL ON otrs.* to 'otrs'@'%' identified by '$OTRS_DB_PASSWORD'";
-
-  $mysqlcmd otrs < ${OTRS_ROOT}scripts/database/otrs-schema.mysql.sql
-  $mysqlcmd otrs < ${OTRS_ROOT}scripts/database/otrs-initial_insert.mysql.sql
-fi
+#$mysqlcmd -e 'use otrs'
+#if [ $? -ne 0  ]; then
+#  $mysqlcmd -e "CREATE DATABASE IF NOT EXISTS otrs;"
+#  $mysqlcmd -e " GRANT ALL ON otrs.* to 'otrs'@'%' identified by '$OTRS_DB_PASSWORD'";
+#
+#  $mysqlcmd otrs < ${OTRS_ROOT}scripts/database/otrs-schema.mysql.sql
+#  $mysqlcmd otrs < ${OTRS_ROOT}scripts/database/otrs-initial_insert.mysql.sql
+#fi
 
 ${OTRS_ROOT}bin/otrs.SetPermissions.pl --otrs-user=otrs --web-group=apache /opt/otrs
 
-echo -e "Setting password for default admin account root@localhost..."
+#echo -e "Setting password for default admin account root@localhost..."
 #${OTRS_ROOT}bin/otrs.SetPassword.pl --agent root@localhost $OTRS_ROOT_PASSWORD
-sudo -u otrs ${OTRS_ROOT}bin/otrs.Console.pl Admin::User::SetPassword root@localhost $OTRS_ROOT_PASSWORD
+#sudo -u otrs ${OTRS_ROOT}bin/otrs.Console.pl Admin::User::SetPassword root@localhost $OTRS_ROOT_PASSWORD
 
-${OTRS_ROOT}bin/Cron.sh start otrs &
-wait
-#${OTRS_ROOT}bin/otrs.Scheduler.pl -w 1 &
-sudo -u otrs ${OTRS_ROOT}bin/otrs.Daemon.pl start
-wait
-#${OTRS_ROOT}bin/otrs.RebuildConfig.pl &
-sudo -u otrs ${OTRS_ROOT}bin/otrs.Console.pl Maint::Config::Rebuild
-wait
-sudo -u otrs ${OTRS_ROOT}bin/otrs.Console.pl Maint::Cache::Delete
-
+#${OTRS_ROOT}bin/Cron.sh start otrs &
 #wait
-#${OTRS_ROOT}bin/otrs.DeleteCache.pl
-#${OTRS_ROOT}bin/otrs.RebuildTicketIndex.pl &
-
-#too slow for running this at restart
-#sudo -u otrs ${OTRS_ROOT}bin/otrs.Console.pl Maint::Ticket::EscalationIndexRebuild
-#sudo -u otrs ${OTRS_ROOT}bin/otrs.Console.pl Maint::Ticket::FulltextIndexRebuild
+#${OTRS_ROOT}bin/otrs.Scheduler.pl -w 1 &
 
 mkdir -p /var/log/otrs
 touch /var/log/otrs/otrs.log
@@ -64,9 +50,55 @@ chown -R otrs:apache /var/log/otrs
 touch /var/log/otrs.log
 chown otrs:apache /var/log/otrs.log
 
-sudo -u otrs ${OTRS_ROOT}bin/otrs.Console.pl Admin::Package::ReinstallAll >> /var/log/otrs/otrs.log
 
-wait
+if [ -z "$MASTER" ]
+then
+      echo "this is a SLAVE instance"
+else
+
+      $mysqlcmd -e 'use otrs'
+        if [ $? -ne 0  ]; then
+          $mysqlcmd -e "CREATE DATABASE IF NOT EXISTS otrs;"
+          $mysqlcmd -e " GRANT ALL ON otrs.* to 'otrs'@'%' identified by '$OTRS_DB_PASSWORD'";
+
+          $mysqlcmd otrs < ${OTRS_ROOT}scripts/database/otrs-schema.mysql.sql
+          $mysqlcmd otrs < ${OTRS_ROOT}scripts/database/otrs-initial_insert.mysql.sql
+        fi
+
+      echo "this is a MASTER instance"
+      echo -e "Setting password for default admin account root@localhost..."
+      sudo -u otrs ${OTRS_ROOT}bin/otrs.Console.pl Admin::User::SetPassword root@localhost $OTRS_ROOT_PASSWORD
+      ${OTRS_ROOT}bin/Cron.sh start otrs &
+      wait
+      sudo -u otrs ${OTRS_ROOT}bin/otrs.Daemon.pl start &
+      echo reinstalling packages
+      sudo -u otrs ${OTRS_ROOT}bin/otrs.Console.pl Admin::Package::ReinstallAll >> /var/log/otrs/otrs.log
+      echo done
+
+      echo rebuilding Configuration
+      sudo -u otrs ${OTRS_ROOT}bin/otrs.Console.pl Maint::Config::Rebuild
+      echo done
+fi
+
+echo reinstalling packages
+sudo -u otrs ${OTRS_ROOT}bin/otrs.Console.pl Admin::Package::ReinstallAll >> /var/log/otrs/otrs.log
+echo done
+
+#${OTRS_ROOT}bin/otrs.RebuildConfig.pl &
+#echo rebuilding Configuration
+#sudo -u otrs ${OTRS_ROOT}bin/otrs.Console.pl Maint::Config::Rebuild
+#echo done
+#wait
+#sudo -u otrs ${OTRS_ROOT}bin/otrs.Console.pl Maint::Cache::Delete
+
+#too slow for running this at restart
+#sudo -u otrs ${OTRS_ROOT}bin/otrs.Console.pl Maint::Ticket::EscalationIndexRebuild
+#sudo -u otrs ${OTRS_ROOT}bin/otrs.Console.pl Maint::Ticket::FulltextIndexRebuild
+
+#echo reinstalling packages
+#sudo -u otrs ${OTRS_ROOT}bin/otrs.Console.pl Admin::Package::ReinstallAll >> /var/log/otrs/otrs.log
+#echo done
+#wait
 
 cat /ssl/server.crt > /etc/pki/tls/certs/star-eionet2012.crt
 cat /ssl/server.key > /etc/pki/tls/private/star-eionet2012.key
@@ -87,13 +119,13 @@ do
 done
 ( cd /etc/mail ; make )  &
 wait
-sed -e 's#  *#\n#g'  <<< "$MAIL_ADDRESSES" | sed -e 's#.*@##' |sort|uniq > /etc/mail/local-host-names &
-wait
+sed -e 's#  *#\n#g'  <<< "$MAIL_ADDRESSES" | sed -e 's#.*@##' |sort|uniq > /etc/mail/local-host-names #&
+#wait
 
-yes | cp -rf /.procmailrc /opt/otrs/.procmailrc &
-wait
-sed "s#TRUSTED_DOMAIN#$TRUSTED_DOMAIN#g" -i /opt/otrs/.procmailrc &
-wait
+yes | cp -rf /.procmailrc /opt/otrs/.procmailrc #&
+#wait
+sed "s#TRUSTED_DOMAIN#$TRUSTED_DOMAIN#g" -i /opt/otrs/.procmailrc #&
+#wait
 sed "s#300px#3000px#g" -i /opt/otrs/var/httpd/htdocs/js/Core.Agent.js
 
 for filename in /opt/otrs/var/httpd/htdocs/skins/Agent/default/css/*.css; do
@@ -107,4 +139,10 @@ done
 
 rm -rf /opt/otrs/var/httpd/htdocs/skins/Agent/default/css-cache/*
 
-supervisord -c /etc/supervisord.d/otrs.ini
+if [ -z "$MASTER" ]
+then
+     supervisord -c /etc/supervisord.d/otrs-slave.ini
+else
+     supervisord -c /etc/supervisord.d/otrs-master.ini
+fi
+#supervisord -c /etc/supervisord.d/otrs.ini
